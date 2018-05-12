abb1053b8481cbb27a77b69fcbec208c914dd1c6
Fabien Cortina <fabien.cortina@gmail.com>
2018-05-12 18:56:35 +0900

    Use RPC client from other project to fix bugs
    I should still to a more standard framework ASAP

--- src/main/webapp/utils/RPC.js
+++ src/main/webapp/utils/RPC.js
@@ -1,62 +1,62 @@
 // @flow
-/* eslint no-console: ["off"] */
-/* eslint no-unused-vars: ["off"] */
 "use strict";
-
-// TODO : replace with standard Promise
-import when from "when";
 
 // TODO : also, replace with standard framework (fetch?)
 export default class RPC {
 
-    get(path: string): Promise<string> {
-        const deferred = when.defer();
-        this.open("GET", path)
-            .then(resolveResponse.bind(null, deferred))
-            .catch((e) => deferred.reject(e));
-        return deferred.promise;
+    get(path: string): Promise<any> {
+        return this._open("GET", path)
+            .then((data) => deserializeData(data));
     }
 
-    post(path: string, data: {[key: string]: any}): Promise<any> {
-        const deferred = when.defer();
-        const json = JSON.stringify(data);
-        this.open("POST", path, json)
-            .then(resolveResponse.bind(null, deferred))
-            .catch((e) => deferred.reject(e));
-        return deferred.promise;
+    post(path: string, data: any): Promise<any> {
+        return serializeData(data)
+            .then((json) => this._open("POST", path, json, "application/json"))
+            .then((data) => deserializeData(data));
     }
 
-    _delete(path: string, data: {[key: string]: any}): Promise<any> {
-        const deferred = when.defer();
-        const json = JSON.stringify(data);
-        this.open("DELETE", path, json)
-            .then(resolveResponse.bind(null, deferred))
-            .catch((e) => deferred.reject(e));
-        return deferred.promise;
+    _delete(path: string, data: any): Promise<any> {
+        return serializeData(data)
+            .then((json) => this._open("DELETE", path, json, "application/json"))
+            .then((data) => deserializeData(data));
     }
 
-    open(method: string, path: string, data?: any): Promise<any> {
-        const deferred = when.defer();
-        try {
-            const xhr = getXMLHttpRequest();
-            console.debug(`Sending ${method} to ${path}`);
-            xhr.open(method, path, true);
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4) {
-                    console.debug(`Handling response from ${method}: ${xhr.status}`);
-                    if (xhr.status == 200) {
-                        deferred.resolve(xhr.response);
-                    } else {
-                        const errorMessage = extractErrorMessage(xhr.status, xhr.response);
-                        deferred.reject(new Error(errorMessage));
-                    }
+    _open(method: string, path: string, data?: any, contentType?: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            try {
+                const xhr = getXMLHttpRequest();
+                console.debug(`Sending ${method} to ${path}`);
+                xhr.withCredentials = true;
+                xhr.open(method, path, true);
+                if (contentType) {
+                    xhr.setRequestHeader("Content-type", contentType);
                 }
-            };
-            xhr.send(data);
-        } catch (e) {
-            deferred.reject(e);
-        }
-        return deferred.promise;
+                xhr.onreadystatechange = () => {
+                    if (xhr.readyState === 4) {
+                        console.debug(`Handling response from ${method}: ${xhr.status}`);
+                        if (xhr.status === 200) {
+                            resolve(xhr.response);
+                        } else if (xhr.status === 204) {
+                            resolve("");
+                        } else {
+                            let error = extractError(xhr);
+                            if (isDevelopmentProxyError(error)) {
+                                reject(new Error("Can't proxy request"));
+                            } else {
+                                reject(error);
+                            }
+                        }
+                    }
+                };
+                xhr.send(data);
+            } catch (e) {
+                if (isDevelopmentProxyError(e)) {
+                    reject(new Error("Can't proxy request"));
+                } else {
+                    reject(e);
+                }
+            }
+        });
     }
 }
 
@@ -72,24 +72,52 @@ function getXMLHttpRequest(): XMLHttpRequest {
     }
 }
 
+function serializeData(data: any): Promise<string> {
+    try {
+        const json = JSON.stringify(data);
+        return Promise.resolve(json);
+    } catch (e) {
+        return Promise.reject(e);
+    }
+}
+
+function deserializeData(data: any): Promise<string> {
+    try {
+        if (isJSON(data)) {
+            return Promise.resolve(JSON.parse(data));
+        } else {
+            return Promise.resolve(data);
+        }
+    } catch (e) {
+        return Promise.reject(e);
+    }
+}
+
 function isJSON(string) {
     return string.length > 0 && (string.charAt(0) === "{" || string.charAt(0) === "[");
 }
 
-function extractErrorMessage(statusCode: number, responseContent: string): string {
+function extractError(xhr: XMLHttpRequest): Error {
+    return extractStatusError(xhr.status, xhr.response);
+}
+
+function extractStatusError(statusCode: number, responseContent: string): Error {
     if (statusCode === 0) {
-        return "Could not connect to the server!";
+        return new Error("Could not connect to the server!");
     } else if (statusCode === 500) {
-        return "Internal Server Error";
+        return new Error("Internal Server Error");
     } else {
-        return statusCode + ": " + responseContent;
+        return new Error(statusCode + ": " + responseContent);
     }
 }
 
-function resolveResponse(deferred: {resolve: (any) => {}}, response: any) {
-    if (isJSON(response)) {
-        deferred.resolve(JSON.parse(response));
-    } else {
-        deferred.resolve(response);
+export function isDevelopmentProxyError(error: Error): boolean {
+    try {
+        const isProxyError = error.toString().indexOf("504") >= 0;
+        const host = location.host;
+        const isDevHost = host.startsWith("localhost") || host.startsWith("127") || host.startsWith("0");
+        return isProxyError && isDevHost;
+    } catch (e) {
+        return false;
     }
 }
